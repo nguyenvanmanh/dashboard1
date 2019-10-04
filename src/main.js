63f81802d14496e2bd69091d540c840c9a59be6f
Trần Minh Châu - CMC Global DU1 <tmchau@ldap.cmcglobal.com.vn>
2019-10-04 13:51:46 +0700

    added emailList table

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import VueRouter from "vue-router";
 import dashboard from "./components/dashboard.vue";
 import login from "./components/login.vue";
 import register from "./components/register.vue";
-import sendingEmail from "./components/sendingEmail.vue";
+import sendingEmail from "./components/emailList.vue";
 import VeeValidate from "vee-validate";
 import vuetify from './plugins/vuetify';
-
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 Vue.use(VeeValidate);
 
 Vue.use(VueRouter);
@@ -26,8 +26,11 @@ const router = new VueRouter({
 
 Vue.config.productionTip = false;
 
-new Vue({
+export default new Vue({
   router,
   vuetify,
+  icons: {
+    iconfont: 'md',
+  },
   render: h => h(App)
 }).$mount("#app");
