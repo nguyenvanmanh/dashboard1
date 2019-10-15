3c6ee45656645c3f1a2965289346c98cfbcc9236
Trần Minh Châu - CMC Global DU1 <tmchau@ldap.cmcglobal.com.vn>
2019-10-15 09:08:15 +0700

    fixed edit form

--- src/components/departments.vue
+++ src/components/departments.vue
@@ -14,13 +14,13 @@
           <div class="flex-grow-1"></div>
 
           <v-radio-group v-model="radios" row :mandatory="false">
-            <v-radio label="Active" value="0" >Active</v-radio>
-            <v-radio label="All" value="1" >All</v-radio>
+            <v-radio label="Active" value="0">Active</v-radio>
+            <v-radio label="All" value="1">All</v-radio>
           </v-radio-group>
 
           <v-dialog v-model="dialog" max-width="500px">
             <template v-slot:activator="{ on }">
-              <v-btn color="primary" dark class="mr-2" v-on="on">New Department</v-btn>
+              <v-btn color="primary" dark class="mb-2" v-on="on">New Department</v-btn>
             </template>
 
             <v-card>
@@ -126,7 +126,7 @@ export default {
     let self = this; //closure
 
     axios
-      .get("http://192.168.9.119:8081/rest/getListDepartmentActive")
+      .get("http://192.168.9.171:8081/rest/getListDepartmentActive")
 
       .then(function(response) {
         self.departments = response.data;
@@ -172,7 +172,7 @@ export default {
 
   methods: {
     editDept(dept) {
-      this.editedI19ndex = this.departments.indexOf(dept);
+      this.editedIndex = this.departments.indexOf(dept);
       this.editedDept = Object.assign({}, dept);
       this.dialog = true;
     },
@@ -184,7 +184,7 @@ export default {
       confirm("Are you sure you want to delete this department?") &&
         axios
           .post(
-            `http://192.168.9.119:8081/rest/inActiveDepartment`,
+            `http://192.168.9.171:8081/rest/inActiveDepartment`,
             this.editedDept
           )
           .then(response => {
@@ -216,7 +216,7 @@ export default {
         Object.assign(this.departments[this.editedIndex], this.editedDept);
         axios
           .post(
-            "http://192.168.9.119:8081/rest/updateDepartmentInfomation",
+            "http://192.168.9.171:8081/rest/updateDepartmentInfomation",
             this.editedDept
           )
           .then(response => {
@@ -235,7 +235,7 @@ export default {
         this.departments.push(this.editedDept);
         axios
           .post(
-            "http://192.168.9.119:8081/rest/insertDepartment",
+            "http://192.168.9.171:8081/rest/insertDepartment",
             this.editedDept
           )
           .then(response => {
@@ -255,7 +255,7 @@ export default {
       if (this.radios === "0") {
         console.log("made it to active");
         axios
-          .get("http://192.168.9.119:8081/rest/getListDepartmentActive")
+          .get("http://192.168.9.171:8081/rest/getListDepartmentActive")
 
           .then(function(response) {
             self.departments = response.data;
@@ -265,12 +265,10 @@ export default {
             // eslint-disable-next-line
             console.log(err);
           });
-   
-      }
-    else {
+      } else {
         console.log("made it to all");
         axios
-          .get("http://192.168.9.119:8081/rest/getAllListDepartment")
+          .get("http://192.168.9.171:8081/rest/getAllListDepartment")
 
           .then(function(response) {
             self.departments = response.data;
