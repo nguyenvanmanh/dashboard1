d7664d287951ae98b0fd54aa16011301412fb4c2
naphuong <palleducanh10@gmail.com>
2019-10-31 09:56:12 +0700

    update campaign screen

--- src/components/campaigns.vue
+++ src/components/campaigns.vue
@@ -4,54 +4,151 @@
     <v-layout>
       <v-flex>
         <v-data-table
-          :headers="headers"
+          :headers="headers.campaigns"
           :items="campaigns"
+          item-key="campaignId"
           v-model="selected"
           class="elevation-1"
           data-app
           show-select
         >
+          <template v-slot:item.stt="{item}">
+            <span> {{campaigns.map( (item) => item.campaignId).indexOf(item.campaignId) + 1 }}</span>
+          </template>
+
           <!-- New Item popup -->
           <template v-slot:top>
             <v-toolbar flat color="white">
               <v-toolbar-title>Campaing Management</v-toolbar-title>
               <v-divider class="mx-4" inset vertical></v-divider>
+
+              <!-- Dialog button add new campaign -->
+              <v-dialog v-model="dialogs.addCampaign" persistent max-width="600px">
+                <template v-slot:activator="{ on }">            
+                  <v-btn small dark color="blue lighten-2" v-on="on"><v-icon small>library_add</v-icon></v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">Create New Campaign</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-col>
+                        <v-row cols="12" sm="6" md="4">
+                          <v-text-field v-model="newCampaign.title" label="Campaign Name"></v-text-field>
+                        </v-row>
+                        <v-row cols="12" sm="6" md="4">
+                          <v-text-field v-model="newCampaign.description" label="Description"></v-text-field>
+                        </v-row>
+                        <v-row>
+                          <v-col cols="12" lg="6">
+                            <v-menu
+                              :close-on-content-click="false"
+                              transition="scale-transition"
+                              offset-y
+                              max-width="290px"
+                            >
+                              <template v-slot:activator="{ on }">
+                                <v-text-field
+                                  v-model="dateFormated"
+                                  label="Duration"                         
+                                  prepend-icon="event"
+                                  readonly
+                                  v-on="on"
+                                ></v-text-field>
+                              </template>
+                              <v-date-picker v-model="dates" scrollable range no-title ></v-date-picker>
+                            </v-menu>                            
+                        </v-col>
+                         <v-col cols="6">
+                          <v-select
+                            v-model="selectedTemplate"
+                            :items="emailTemplates.titles"
+                            menu-props="auto"
+                            label="Email Template"
+                            hide-details
+                            single-line
+                          ></v-select>
+                        </v-col>
+                      </v-row>
+                      </v-col>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="dialogs.addCampaign = false">Close</v-btn>
+                    <v-btn color="blue darken-1" text @click="createCampaign">Create</v-btn>
+                  </v-card-actions>
+                </v-card>
+
+              </v-dialog>
+              <!-- End of Dialog button add new campaign -->
+              
               <div class="flex-grow-1"></div>
-                  <v-btn color="primary" dark @click ="asd">Send All</v-btn>
-     
+              <!-- Send email for selected campaigns button -->
+              <v-btn color="primary" dark @click="sendAll">Send All</v-btn>
+
             </v-toolbar>
           </template>
           <!-- End of New Item popup -->
 
+          <!-- Action icons  -->
           <template v-slot:item.action="{ item }">
-            <v-icon color="orange darken-2" left >rounded_corner</v-icon>
-            <v-icon color="green darken-2"  @click="showDialog">supervised_user_circle</v-icon>
+            <v-icon color="orange darken-2" @click="templateDialog(item.campaignId)" left >rounded_corner</v-icon>
+            <v-dialog v-model="dialogs.listCustomer" scrollable persistent max-width="600px">
+              <template v-slot:activator>
+                <v-icon color="green darken-2"  @click="showDialog">supervised_user_circle</v-icon>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">Choose customers for this campaign</span>
+                </v-card-title>
+
+                <v-data-table 
+                  :headers="headers.customers"
+                  :items="customers"
+                  v-model="selectedCustomer"
+                  item-key="customerEmail"
+                  show-select
+                  hide-default-footer          
+                >
+                </v-data-table>
+
+                <v-card-actions>
+                  <!-- <span>{{ selectedCustomer }}</span> -->
+                  
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="dialogs.listCustomer = false">Close</v-btn>
+                  <v-btn color="blue darken-1" text @click="dialogs.listCustomer = false">Save</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog> 
+            <!-- end of select customer -->
+            
             <v-icon color="teal darken-2" right >email</v-icon>
           </template>
+          <!-- End of Action Icons -->
           
         </v-data-table>
-        <v-dialog v-model="listCustomerDialog" persistent max-width="600px">
+        <!-- select list customer for sending email -->
+        
+
+        <!-- change Template for campaign -->
+        <v-dialog v-model="dialogs.changeTemplate" scrollable max-width=600px>
           <v-card>
             <v-card-title>
-              <span class="headline">Choose customers for this campaign</span>
+              <span class="headline">Choose template email for this campaign</span>
             </v-card-title>
-
-            <v-data-table
-              :headers="customerHeaders"
-              :items="customers"
-              item-key="customerEmail"
-              show-select
-              hide-default-footer          
-            >
-            </v-data-table>
+            
 
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="listCustomerDialog = false">Close</v-btn>
-              <v-btn color="blue darken-1" text @click="listCustomerDialog = false">Save</v-btn>
+              <v-btn color="blue darken-1" text @click="dialogs.changeTemplate = false">Close</v-btn>
+              <v-btn color="blue darken-1" text @click="dialogs.changeTemplate = false">Save</v-btn>
             </v-card-actions>
           </v-card>
-        </v-dialog> 
+
+        </v-dialog>
       </v-flex>
     </v-layout>
   </v-app>
@@ -60,111 +157,121 @@
 <script>
 
 import axios from "axios";
+
+const baseUrl = 'http://localhost:8081';
 export default {
   data() {
     return {
+      dates: [],
+      selectedTemplate: null,
+      emailTemplates: {},
+      newCampaign: {},
       selected: [],
       customers: [],
-      customerHeaders: [
-        {text: "Name", value:"firstName"},
-        {text: "Email", value:"customerEmail"},
-      ],
-      selectedCustomer: [],
+      selectedCustomer: {},
       campaigns: [],
-      dialog: false,
-      listCustomerDialog: false,
-      modal: false,
-      check_activate: false,
-      check_inactivate: false,
-      show1: false,
+      dialogs: {
+        addCampaign: false,
+        changeTemplate: false,
+        listCustomer: false,
+      },
       rules: {
         required: value => !!value || "Required."
       },
-      headers: [
-        { text:"STT", align:"center", value:"stt" },
-        { text:"Id", align:"center", value:"id" },
-        { text:"Campaign Name", align:"center", value:"campaignName" },
-        { text:"Duration From", align:"center", value:"durationFrom" },
-        { text:"Duration To", align:"center", value:"durationTo" },
-        { text:"Description", align:"center", value:"description", sortable:false },
-        { text:"Action", align:"center", value:"action", sortable:false },
-      ],     
+      headers: {
+        customers: [
+          {text: "Name", value:"fullName"},
+          {text: "Email", value:"customerEmail"},
+        ],
+        campaigns: [
+          { text:"STT", align:"center", value:"stt" },
+          { text:"Id", align:"center", value:"campaignId" },
+          { text:"Campaign Name", align:"center", value:"title" },
+          { text:"Duration From", align:"center", value:"startDate" },
+          { text:"Duration To", align:"center", value:"endDate" },
+          { text:"Description", align:"center", value:"description", sortable:false },
+          { text:"Action", align:"center", value:"action", sortable:false },
+        ], 
+      }
     };
   },
-
+  computed: {
+    dateFormated(){
+      return this.dates.join('~');
+    }
+  },
   created() {
-    this.initialize();
+    axios.get(`${baseUrl}/email/get-all-topic`)
+      .then(res => {
+        this.emailTemplates.titles = res.data.map( item => item.title );
+        this.emailTemplates.ids = res.data.map(item => item.emaiTemplateId);
+        } )
+      .catch(err =>{ 
+        // TODO: log this err
+      });
+    axios.get(`${baseUrl}/email/getAllCampaignDetail`)
+      .then(res => {
+          res.data.map(item => {
+            item.campaign.startDate = item.campaign.startDate.substring(0,10);
+            item.campaign.endDate = item.campaign.endDate.substring(0,10);
+            this.campaigns.push(item.campaign)
+          });
+          this.customers =  res.data[0].customerList;
+          this.customers.map( customer => customer.fullName = customer.firstName + ' ' + customer.lastName );
+        });
+        
   },
   methods: {
-    asd(){ alert(2)},
-    fetchUsers() {
-      axios
-        .get("http://localhost:3000/user", {
-          headers: { Authorization: localStorage.getItem("tocken") }
-        })
-        .then(response => {
-          this.users = response.data;
+    templateDialog(id){
+      this.dialogs.changeTemplate = true;
+    },
+    createCampaign(){
+      // get template id for new campaign
+      this.emailTemplates.titles.forEach((element,id) => {
+        if(this.selectedTemplate === element)
+          this.newCampaign.emailTemplateId = this.emailTemplates.ids[id];
+      });
+      // get start date and due date
+      this.dates.sort();
+      this.newCampaign.startDate = this.dates[0];
+      this.newCampaign.endDate = this.dates[1];
+      axios.post(`${baseUrl}/email/add-campaign`,this.newCampaign)
+        .then(res =>{
+          if(res.status === 200);
+          {
+            this.newCampaign.campaignId = res.data;
+            this.campaigns.push(this.newCampaign);
+            this.dates = [];
+            this.newCampaign ={};  
+            this.dialogs.addCampaign = false;
+          }
         });
-    },
-    initialize() {
-      axios.get('http://169.254.138.3:8081/email/getAllCampaignDetail')
-      .then(res => {
-        console.log(res.data);
-        this.customers = res.data})
-      .catch(err => console.err(err));
-
-     this.customers = [
-       {name: "john doe" , email: "jonh@jirin.com"},
-       {name: "jane doe" , email: "jane@hotmail.com"},
-       {name: "mark kize" , email: "markh@rapme.com"},
-       {name: "mike martin" , email: "mike@gor.cc"},
-     ];
-     this.campaigns = [
-        {
-          stt: 1,
-          id: 2,
-          campaignName: "summer slam",
-          durationFrom: new Date().getDate(),
-          durationTo: new Date().getDate() + 1,
-          description: "lorem islum mode te: Counting objects: 10 Total 5 (delta 3), reused 5 (delta 3), pack-reused 0"
-        },
-        {
-          stt: 2,
-          id: 0,
-          campaignName: "blackfriday",
-          durationFrom: new Date().getDate(),
-          durationTo: new Date().getDate() + 2,
-          description: "lorem islum mode te: Counting objects: 10 Total 5 (delta 3), reused 5 (delta 3), pack-reused 0"
-        },
-        {
-          stt: 3,
-          id: 9,
-          campaignName: "blackfriday",
-          durationFrom: new Date().getDate(),
-          durationTo: new Date().getDate() + 2,
-          description: "lorem islum mode te: Counting objects: 10 Total 5 (delta 3), reused 5 (delta 3), pack-reused 0"
-        },
-        {
-          stt: 4,
-          id: 8,
-          campaignName: "blackfriday",
-          durationFrom: new Date().getDate(),
-          durationTo: new Date().getDate() + 2,
-          description: "lorem islum mode te: Counting objects: 10 Total 5 (delta 3), reused 5 (delta 3), pack-reused 0"
-        },
-        
-      ];
-    },
-    showDialog() {
-      this.listCustomerDialog = true;
-    },
-    close() {
-      this.dialog = false;
-    },
-    save() {
       
-      this.close();
-    }
+    },
+    // fetchUsers() {
+    //   axios
+    //     .get(`${baseUrl}/user`, {
+    //       headers: { Authorization: localStorage.getItem("tocken") }
+    //     })
+    //     .then(response => {
+    //       this.users = response.data;
+    //     });
+    // },
+    // List customer
+    showDialog() {
+      this.dialogs.listCustomer = true;
+    },
+
+    sendAll(id){
+      this.selected.map( item => {
+        axios.post(`${baseUrl}/sendMail`, {
+          customers: selectedCustomer,
+          campaignId: item.campaignId,
+          sendEmailUserId: 1
+          
+        });
+      });
+    },
   }
 };
 
