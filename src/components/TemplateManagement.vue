58307c40d669492c2445847a7420f75df6c7cc64
Nguyễn Hồng Đông - CMC Global DU1 <nhdong@ldap.cmcglobal.com.vn>
2019-11-05 17:09:07 +0700

    change table

--- src/components/TemplateManagement.vue
+++ src/components/TemplateManagement.vue
@@ -1,90 +1,107 @@
 <template>
   <v-app id="inspire">
-    <v-data-table
-      :headers="headers"
-      :items="dataTemplates"
-      sort-by="calories"
-      class="elevation-1"
-      :search="search"
-    >
-      <template v-slot:item.index="{ item }">
-        <!-- {{dem}} -->
-        {{dataTemplates.map(function(x) {
-        return x.id;
-        }).indexOf(item.id)+1}}
-      </template>
-      <template v-slot:top>
-        <v-toolbar flat color="white">
-          <v-toolbar-title>Templates Management</v-toolbar-title>
-          <v-divider class="mx-4" inset vertical></v-divider>
-          <!-- <div class="flex-grow-1"></div> -->
-
-          <v-dialog v-model="dialog" max-width="800px">
-            <!-- <template v-slot:activator="{ on }">
+    <div class="row">
+      <div class="col-xl-12">
+        <div id="panel-1" class="panel">
+          <div class="panel-hdr">
+            <span >
+              <h3>
+                Template
+                <span class="fw-100">
+                  <i>Table</i>
+                </span>
+              </h3>
+            </span>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-dialog v-model="dialog" max-width="800px">
+              <!-- <template v-slot:activator="{ on }">
               <v-btn color="primary" dark class="mb-2" v-on="on">New Template</v-btn>
-            </template>-->
-            <template v-slot:activator="{ on }">
-              <v-btn small dark color="blue lighten-2" v-on="on">
-                <v-icon small>library_add</v-icon>
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="headline">{{ formTitle }}</span>
-              </v-card-title>
+              </template>-->
+              <template v-slot:activator="{ on }">
+                <v-btn small dark color="blue lighten-2" v-on="on">
+                  <v-icon small>library_add</v-icon>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">{{ formTitle }}</span>
+                </v-card-title>
 
-              <v-card-text>
-                <v-container>
-                  <v-row v-model="row_input">
-                    <v-col cols="12">
-                      <label class="form-label" for="simpleinputInvalid">Title</label>
-                      <input
-                        type="text"
-                        v-bind:class="[formControl, isInvalid]"
-                        id="simpleinputInvalid"
-                        v-model="title_input"
-                        required
-                      />
-                      <div
-                        class="invalid-feedback"
-                        :style="{display: titleValidate}"
-                      >Please enter text in here.</div>
-                    </v-col>
-                    <v-col cols="12">
-                      <label class="form-label" for="example-password">Body</label>
-                    </v-col>
-                    <v-col cols="12">
-                      <vue-editor label="Body" v-model="body_input"></vue-editor>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
+                <v-card-text>
+                  <v-container>
+                    <v-row v-model="row_input">
+                      <v-col cols="12">
+                        <label class="form-label" for="simpleinputInvalid">Title</label>
+                        <input
+                          type="text"
+                          v-bind:class="[formControl, isInvalid]"
+                          id="simpleinputInvalid"
+                          v-model="title_input"
+                          required
+                        />
+                        <div
+                          class="invalid-feedback"
+                          :style="{display: titleValidate}"
+                        >Please enter text in here.</div>
+                      </v-col>
+                      <v-col cols="12">
+                        <label class="form-label" for="example-password">Body</label>
+                      </v-col>
+                      <v-col cols="12">
+                        <vue-editor label="Body" v-model="body_input"></vue-editor>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-
-          <!-- Implement search bar-->
-          <v-spacer></v-spacer>
-          <v-text-field
-            v-model="search"
-            append-icon="search"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-          <v-divider class="mx-4" inset vertical></v-divider>
-          <!--End searchbar-->
-        </v-toolbar>
-      </template>
-      <template v-slot:item.action="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)">edit</v-icon>
-      </template>
-    </v-data-table>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                  <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </div>
+          <div class="panel-container show">
+            <div class="panel-content">
+              <!-- datatable start -->
+              <table
+                id="dt-basic-example"
+                class="table table-bordered table-hover table-striped w-100"
+              >
+                <thead>
+                  <tr>
+                    <th class="text-center" style="width: 5%">#</th>
+                    <th class="text-center">Title</th>
+                    <th class="text-center">Body</th>
+                    <th class="text-center" style="width: 15%">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr  v-for="(template,i) in dataTemplates" :key="i" class="text-center">
+                    <td>{{i+1}}</td>
+                    <td>{{template.title}}</td>
+                    <td >{{template.body}}</td>
+                   
+                    <td>
+                      <a
+                        @click="editItem(template)"
+                        class="btn btn-sm btn-outline-primary mr-2"
+                        title="Edit"
+                      >
+                        <i class="fal fa-edit"></i> Edit
+                      </a>
+                    </td>
+                  </tr>
+        
+                </tbody>
+              </table>
+              <!-- datatable end -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <alert-action :message="messageAlert" :typeAlert="typeAlert" :show="show"></alert-action>
   </v-app>
@@ -99,6 +116,7 @@ import AlertAction from "./share/Alert";
 const base_url = API.BASEURL;
 export default {
   data: () => ({
+    headerTable :['#',"Title","Body","Action"],
     show: true,
     typeAlert: "",
     messageAlert: "",
@@ -163,6 +181,7 @@ export default {
         .get(`${base_url}/email/get-all-topic`)
         .then(response => {
           this.dataTemplates = response.data;
+          console.log(this.dataTemplates)
         })
         .catch(error => {
           console.log(error);
@@ -187,6 +206,7 @@ export default {
       this.body_input = "";
       this.titleValidate = "none";
       this.isInvalid = "";
+      this.id_template="";
       this.closeAlert();
     },
 
@@ -204,7 +224,7 @@ export default {
           this.isInvalid = "is-invalid";
         } else {
           data = {
-            emailTemplateId: this.id_template,
+            id: this.id_template,
             title: this.title_input,
             body: this.body_input
           };
@@ -257,5 +277,8 @@ export default {
   position: fixed;
   bottom: 0;
   right: 0;
+}
+.text-center{
+  text-align: center
 }
 </style>
